17caed55feddab1ee6225548fded75be0709b0d4
Baskanbetul <btlbskn@hotmail.com>
2022-07-20 09:51:31 -0600

    Make Card functional component to display

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Card.css';
+
+const Card = ({name,date,time,number}) => {
+  return (
+    <div className='Card'>
+      <h2>{name}</h2>
+      <p>{date}</p>
+      <p>{time}</p>
+      <p>{number}</p>
+      <button>Cancel</button>
+    </div>
+  )
+}
+
+export default Card
